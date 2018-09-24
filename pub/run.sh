31983e15321e967cefc32688e68819a718ac7c76
nickfloop <nick@forward-loop.com>
2018-09-24 20:54:57 +0800

    [FIX] pub/sub cert name typo

--- pub/run.sh
+++ pub/run.sh
@@ -2,6 +2,6 @@
 while :
 do
     echo 'Publishing to test/test'
-    mosquitto_pub -h broker -p ${PUB_PORT} -t 'test/test' -m "$("date")" -d --cert /sub/client.pem --key /sub/client.key    
+    mosquitto_pub -h broker -p ${PUB_PORT} -t 'test/test' -m "$("date")" -d --cert /sub/client.crt --key /sub/client.key    
     sleep 2s
 done
